891dd05a1b810bc5df48c78411bc90b8d2b5d73f
Qian Wang <wangqian5730@gmail.com>
2018-11-16 12:21:49 +0800

    temorary solution for parallel beam search

--- beaver/infer/translator.py
+++ beaver/infer/translator.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import torch
 import torch.nn as nn
-from beaver.infer.beam import Beam
 
 
 def beam_search(opt, model, batch, fields, device):
@@ -14,9 +13,6 @@ def beam_search(opt, model, batch, fields, device):
 
     pad = fields["tgt"].pad_id
     eos = fields["tgt"].eos_id
-
-    beams = [Beam(opt.beam_size, fields["tgt"].pad_id, fields["tgt"].bos_id, fields["tgt"].eos_id,
-                  device, opt.length_penalty) for _ in range(batch_size)]
 
     def repeat_beam(tensor):
         dim = tensor.dim()
@@ -35,14 +31,20 @@ def beam_search(opt, model, batch, fields, device):
     alive_scores = torch.full([batch_size, beam_size], -1e20).float()
     alive_scores[:, 0] = 0.
 
+    finish_hypotheses = torch.full([batch_size * beam_size, 1], fill_value=pad).long()
+    finish_scores = torch.full([batch_size, beam_size], -1e20).float()
+
     beam_index_expander = torch.arange(batch_size).unsqueeze(1) * beam_size
     length_penalty = (6 / (5 + torch.arange(opt.max_length).float())) ** opt.length_penalty
-    inf = torch.tensor(-1e20).to(device)
+    inf = torch.tensor(-1e20).float().to(device)
 
     alive_hypotheses = alive_hypotheses.to(device)
     alive_scores = alive_scores.to(device)
     beam_index_expander = beam_index_expander.to(device)
     length_penalty = length_penalty.to(device)
+
+    finish_hypotheses = finish_hypotheses.to(device)
+    finish_scores = finish_scores.to(device)
 
     finished = [[] for _ in range(batch_size)]
 
@@ -56,21 +58,34 @@ def beam_search(opt, model, batch, fields, device):
         out = generator(dec_out).view(batch_size, beam_size, -1)
         beam_scores = alive_scores.unsqueeze(-1) + out
 
-        # [batch, beam]
-        alive_scores, index = beam_scores.view(batch_size, -1).topk(beam_size)
-        origin = index / num_words
-        tokens = index - origin * num_words
+        # 1. select 2 x beam candidates
+        # [batch, 2 x beam]
+        top_scores, index = beam_scores.view(batch_size, -1).topk(beam_size)
+        origin = index // num_words
+        tokens = index % num_words
 
         origin = (origin + beam_index_expander).view(-1)
+        candidate_seqs = alive_hypotheses.index_select(0, origin)
+        candidate_seqs = torch.cat([candidate_seqs, tokens.view(-1, 1)], dim=-1)
+        previous = previous.index_select(0, origin.view(-1))
 
-        hypotheses = torch.index_select(alive_hypotheses, 0, origin)
-        alive_hypotheses = torch.cat([hypotheses, tokens.view(-1).unsqueeze(-1)], dim=-1)
-        previous = previous.index_select(0, origin)
+        # 2. pick out unfinished sequences
+        # [batch, 2 x beam]
+        flags = torch.eq(tokens, eos)
+        alive_masked_scores = top_scores + flags.float() * inf
+        finish_masked_scores = top_scores + (1 - flags.float()) * inf
 
+        # 3. select beam unfinished candidates
+        alive_scores, alive_indices = alive_masked_scores.topk(beam_size)
+        alive_indices = (alive_indices + beam_index_expander).view(-1)
+        alive_hypotheses = candidate_seqs.index_select(0, alive_indices.view(-1))
+        previous = previous.index_select(0, alive_indices.view(-1))
+
+        # 4. select finished sequences
         for j in range(batch_size):
-            for idx, tok in enumerate(alive_hypotheses.view(batch_size, beam_size, -1)[j, :, -1]):
+            for idx, tok in enumerate(candidate_seqs.view(batch_size, beam_size, -1)[j, :, -1]):
                 if tok == eos:
-                    finished[j].append((alive_scores[j, idx].clone(), alive_hypotheses.view(batch_size, beam_size, -1)[j, idx, 1:]))
+                    finished[j].append((top_scores[j, idx].clone(), candidate_seqs.view(batch_size, beam_size, -1)[j, idx, 1:]))
 
         # end condition
         max_score, _ = torch.max(alive_scores.view(batch_size, -1) * length_penalty[i+2], dim=-1)
@@ -82,7 +97,7 @@ def beam_search(opt, model, batch, fields, device):
     result = []
     for j in range(batch_size):
         if not finished[j]:
-            result.append(b.hypotheses[0, 1:])
+            result.append(alive_hypotheses[j, 0, 1:])
         else:
             candidate = sorted(finished[j], key=lambda t: t[0] * length_penalty[t[1].size(0)], reverse=True)
             result.append(candidate[0][1])
